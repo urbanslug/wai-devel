46b3de8ebf8c7860f7b2f142c4d12725f40a09bf
Yann Esposito <yann@vigiglobe.com>
2016-02-29 17:46:53 +0100

    remove warns

--- src/Devel/Compile.hs
+++ src/Devel/Compile.hs
@@ -44,7 +44,7 @@ import Devel.Types
 
 import System.FilePath.Posix (takeExtension, pathSeparator)
 import System.Directory (doesFileExist)
-import Data.List (union, delete, isInfixOf, nub, (\\))
+import Data.List (union, delete, isInfixOf, nub)
 import Data.Maybe (fromMaybe)
 import Control.Monad (filterM)
 
@@ -88,8 +88,8 @@ getSourceList srcDir cabalSrcList = do
       -- nub to remove duplicates yet again.
       sourceList = nub $ delete "app/DevelMain.hs" $ delete "app/devel.hs" sourceList'
 
-  -- Drop leading "./" in filepaths with `map (\x -> x \\ "./") sourceList`
-  return $ map removeLeadingDotSlash sourceList -- sourceList
+  -- Drop leading "./" in filepaths
+  return $ map removeLeadingDotSlash sourceList
     where
         removeLeadingDotSlash ('.':'/':xs) = xs
         removeLeadingDotSlash xs = xs
